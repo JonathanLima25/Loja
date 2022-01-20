a2393bc096a485222a28abebacea72485a7f0078
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-01-20 10:43:56 -0300

    Constantes do site, banco de dados e php

--- controller/Config.class.php
+++ controller/Config.class.php
@@ -1,8 +1,24 @@
 <?php
 
-const SITE_URL = "http://localhost";
-const SITE_PASTA = "loja";
-const SITE_NOME = "Loja do Jonathan - PHP 7 E MySql";
-const SITE_EMAIL = "lojadojonathan@gmail.com";
+class Config{
+
+    //Informações básicas do site
+    const SITE_URL = "http://localhost";
+    const SITE_PASTA = "loja";
+    const SITE_NOME = "Loja do Jonathan - PHP 7 E MySql";
+    const SITE_EMAIL = "lojadojonathan399@gmail.com";
+
+    //Informações do banco de dados
+    const BD_HOST = "localhost",
+        BD_USER = "root",
+        BD_SENHA = "",
+        BD_BANCO = "lojajonathan";
+        
+    //Informações para o PHP mailler
+    const EMAIL_HOST = "smtp.gmail.com";
+
+}
+
+
 
 ?>
