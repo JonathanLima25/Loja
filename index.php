7270c9db0ac041f0b60273bf7f2fafcbacb9b53a
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-01-20 15:33:50 -0300

    Testando a estrutura do projeto

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 require './vendor/autoload.php';
 
 $smarty = new Template();
-
+Rotas::get_pagina();
 
 
 
 //Valores para template
 $smarty->assign('NOME', 'JONATHAN DE LIMA FERREIRA');
+echo Rotas::get_SiteTEMA();
+
 $smarty->display('index.tpl');
 
 ?>
