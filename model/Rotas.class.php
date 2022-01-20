6596fef93df4bb0c45f03783f59d188bfecffbc1
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-01-19 21:54:10 -0300

    adicionando pasta controller a Rota

--- model/Rotas.class.php
+++ model/Rotas.class.php
@@ -4,7 +4,7 @@ class Rotas{
     static function get_pagina()
     {
         if (isset($_GET['pag'])){
-            $pagina = $_GET['pag'].'php';
+            $pagina = 'controler/' .$_GET['pag'].'.php';
             if(file_exists($pagina)){
                 include $pagina;
             }else{
