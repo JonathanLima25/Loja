81c61823351cf5dab41b53604caca79ee491e115
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-01-19 22:50:27 -0300

    Passando rota pelo array

--- model/Rotas.class.php
+++ model/Rotas.class.php
@@ -1,10 +1,16 @@
 <?php
 
 class Rotas{
+
+    public static $pag;
+
     static function get_pagina()
     {
         if (isset($_GET['pag'])){
-            $pagina = 'controler/' .$_GET['pag'].'.php';
+            $pagina = $_GET['pag'];
+            self::$pag = explode('/', $pagina);
+            
+            $pagina = 'controller/' .self::$pag[0].'.php';
             if(file_exists($pagina)){
                 include $pagina;
             }else{
