9a22d39f010768f18db6bd9da04ae89827f4c473
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-01-20 10:26:18 -0300

    Adicionando os arquivos controller dos model para manter padrão MVC e arquivo com constantes de configuração

---
+++ controller/Config.class.php
@@ -0,0 +1,8 @@
+<?php
+
+const SITE_URL = "http://localhost";
+const SITE_PASTA = "loja";
+const SITE_NOME = "Loja do Jonathan - PHP 7 E MySql";
+const SITE_EMAIL = "lojadojonathan@gmail.com";
+
+?>
