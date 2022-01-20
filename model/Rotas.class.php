b1e1ecdbd4b01e99444f77df49c91c594a63d012
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-01-19 21:35:43 -0300

    Criando url's mais amigaveis

--- model/Rotas.class.php
+++ model/Rotas.class.php
@@ -1,4 +1,18 @@
 <?php
 
+class Rotas{
+    static function get_pagina()
+    {
+        if (isset($_GET['pag'])){
+            $pagina = $_GET['pag'].'php';
+            if(file_exists($pagina)){
+                include $pagina;
+            }else{
+                include 'erro.php';
+        }
+        }
+    }
+}
+
 
 ?>
